593286d5ace0cd94831aa93a80cf5be4596d0e00
Kostadin Stanev <stanebvns@gmail.com>
2023-08-08 17:05:01 +0300

    first task

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import RadioForm from './components/RadioForm';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <RadioForm />
+    </div>
+  );
 }
 
 export default App;
