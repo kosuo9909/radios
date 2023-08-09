ee44fc01a462f8acbd6ae542c03f9f5ac3075546
Kostadin Stanev <kostadin.stanev@p3-group.com>
2023-08-09 12:38:14 +0300

    task 4 (Custom Buttons)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import CustomButtons from './components/CustomButtons';
 // import DialogTest from './components/DialogTest';
-import DialogTestVanilla from './components/DialogTestVanilla';
+// import DialogTestVanilla from './components/DialogTestVanilla';
 
 function App() {
   return (
     <div className="App">
-      <DialogTestVanilla />
+      <CustomButtons />
     </div>
   );
 }
