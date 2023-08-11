2f466163db7ef48bf2891f4cdae185f30d03fcd8
Kostadin Stanev <kostadin.stanev@p3-group.com>
2023-08-11 10:01:49 +0300

    app.js conflict commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import CustomButtons from './components/CustomButtons';
-// import DialogTest from './components/DialogTest';
+import DialogTest from './components/DialogTest';
 // import DialogTestVanilla from './components/DialogTestVanilla';
 
 function App() {
   return (
     <div className="App">
       <CustomButtons />
+      <DialogTest />
     </div>
   );
 }
